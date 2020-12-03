5b1b821c247f0faa607550a4374ed9e7821e391a
Mleblanc94 <leblancmichael94@gmail.com>
2020-12-02 22:49:21 -0500

    JS is cleaned up and working

--- script.js
+++ script.js
@@ -4,23 +4,50 @@ let billAmount = document.getElementById("input1").value;
 let serviceLevel = document.getElementById("selectID").value;
 let amountPeople = document.getElementById("input2").value;
 
-let realBillAmount = Number(billAmount);
-let realAmountPeople = Number(amountPeople);
-let realServiceLevel = Number(serviceLevel);
+// VALIDATE INPUT
 
-if (realBillAmount === "" || realAmountPeople == 0) {
+if (billAmount === "" || amountPeople == 0) {
 	alert("Please enter values");
+	return;
 }
 
-let a = (realBillAmount / realAmountPeople) * realServiceLevel;
-
-console.log(a);
-
-
-let giveAnswer = () => {
-document.getElementById("answer").innerHTML = a;
+// CHECK IF THIS INPUT IS EMPTY OR LESS THAN OR EQUAL TO 1
+if (amountPeople === "" || amountPeople <=1) {
+	amountPeople = 1;
+	document.getElementById("each").style.display = "none";
+} else {
+	document.getElementById("each").style.display = "block";
 }
+
+// CALCULATE TIP
+let total = (billAmount / amountPeople) * serviceLevel;
+// ROUND TO TWO DECIMALS
+total = Math.round(total * 100) / 100;
+// NEXT LINE ALLOWS US TO ALWAYS HAVE TWO DIGITS AFTER DECIMAL POINT
+total = total.toFixed(2);
+// DISPLAY THE TIP
+document.getElementById("totalTip").style.display = "block";
+document.getElementById("tip").innerHTML = total;
 }
+
+// HIDE THE TIP AMOUNT ON LOAD
+document.getElementById("totalTip").style.display = "none";
+document.getElementById("each").style.display = "none";
+
+// CLICK TO CALL FUNCTION
+document.getElementById("button").onclick = function() {
+	calculateTip();
+}
+
+// let a = (realBillAmount / realAmountPeople) * realServiceLevel;
+
+// console.log(a);
+
+
+// let giveAnswer = () => {
+// document.getElementById("answer").innerHTML = a;
+// }
+// }
 // let appendAnswer = () => {
 //     let h1 = document.createElement("h1");
 // 	h1.appendChild(document.createTextNode(a));
@@ -32,4 +59,4 @@ document.getElementById("answer").innerHTML = a;
 
 
 
-button.addEventListener("click", giveAnswer);
+// button.addEventListener("click", giveAnswer);
