78c6feebf3c7517050506175efc79deb466b3d97
Mleblanc94 <leblancmichael94@gmail.com>
2021-02-25 22:40:43 -0500

    changes made

--- script.js
+++ script.js
@@ -1,13 +1,13 @@
 //Box capturing
-let box1 = getElementById("box1");
-let box2 = getElementById("box2");
-let box3 = getElementById("box3");
-let box4 = getElementById("box4");
-let box5 = getElementById("box5");
-let box6 = getElementById("box6");
-let box7 = getElementById("box7");
-let box8 = getElementById("box8");
-let box9 = getElementById("box9");
+let box1 = document.getElementById("box1");
+let box2 = document.getElementById("box2");
+let box3 = document.getElementById("box3");
+let box4 = document.getElementById("box4");
+let box5 = document.getElementById("box5");
+let box6 = document.getElementById("box6");
+let box7 = document.getElementById("box7");
+let box8 = document.getElementById("box8");
+let box9 = document.getElementById("box9");
 // Integer I to vary between X and O
 let i = 1;
 
@@ -18,5 +18,5 @@ let box1XO = (i) => {
 	} else {
 		box1.innerHTML = "O";
 	}
-	i++
+	i++;
 }
