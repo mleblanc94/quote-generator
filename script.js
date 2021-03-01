b3623766b94011b1655709e765c583507980f8ab
Mleblanc94 <leblancmichael94@gmail.com>
2021-02-28 21:58:40 -0500

    trying to make it switch between x and o

--- script.js
+++ script.js
@@ -9,7 +9,7 @@ let box7 = document.getElementById("box7");
 let box8 = document.getElementById("box8");
 let box9 = document.getElementById("box9");
 // Integer I to vary between X and O
-let i = 1;
+let i = 2;
 //Letting the players set their names for the game
 let player1 = prompt("Player 1 - Please enter your name:");
 let player2 = prompt("Player 2 - Please enter your name:")
@@ -19,21 +19,20 @@ let box1XO = (i) => {
 		box1.innerHTML = "X";
 	} else {
 		box1.innerHTML = "O";
-	}
-	i++;
+	};
 }
 
 let box2XO = (i) => {
-	if (i % 2) {
+	if (i % 2 === 0) {
 		box2.innerHTML = "X";
 	} else {
 		box2.innerHTML = "O";
-	}
+	};
 	i++;
 }
 
 let box3XO = (i) => {
-	if (i % 2) {
+	if (i % 2 === 0) {
 		box3.innerHTML = "X";
 	} else {
 		box3.innerHTML = "O";
@@ -42,34 +41,34 @@ let box3XO = (i) => {
 }
 
 let box4XO = (i) => {
-	if (i % 2) {
+	if (i % 2 === 0) {
 		box4.innerHTML = "X";
 	} else {
 		box4.innerHTML = "O";
-	}
+	};
 	i++;
 }
 
 let box5XO = (i) => {
-	if (i % 2) {
+	if (i % 2 === 0) {
 		box5.innerHTML = "X";
 	} else {
 		box5.innerHTML = "O";
-	}
+	};
 	i++;
 }
 
 let box6XO = (i) => {
-	if (i % 2) {
+	if (i % 2 === 0) {
 		box6.innerHTML = "X";
 	} else {
 		box6.innerHTML = "O";
-	}
+	};
 	i++;
 }
 
 let box7XO = (i) => {
-	if (i % 2) {
+	if (i % 2 === 0) {
 		box7.innerHTML = "X";
 	} else {
 		box7.innerHTML = "O";
@@ -78,19 +77,19 @@ let box7XO = (i) => {
 }
 
 let box8XO = (i) => {
-	if (i % 2) {
+	if (i % 2 === 0) {
 		box8.innerHTML = "X";
 	} else {
 		box8.innerHTML = "O";
-	}
+	};
 	i++;
 }
 
 let box9XO = (i) => {
-	if (i % 2) {
+	if (i % 2 === 0) {
 		box9.innerHTML = "X";
 	} else {
 		box9.innerHTML = "O";
-	}
+	};
 	i++;
 }
