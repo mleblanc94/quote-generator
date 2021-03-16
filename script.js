7cec098a2c60fc4cf059784bad53218ca1a1831f
Mleblanc94 <leblancmichael94@gmail.com>
2021-03-15 20:22:40 -0400

    added buttons

--- script.js
+++ script.js
@@ -1,7 +1,17 @@
+//Declaring the input screen on the calculator
+
 //Declaring the operations buttons the user can press
 let addition = document.getElementById("addition");
-let addition = document.getElementById("addition");
-let addition = document.getElementById("addition");
-let addition = document.getElementById("addition");
-let addition = document.getElementById("addition");
-let addition = document.getElementById("addition");
+let subtraction = document.getElementById("subtraction");
+let multiplication = document.getElementById("multiplication");
+let division = document.getElementById("division");
+//number declaration
+let seven = document.getElementById("seven");
+let eight = document.getElementById("eight");
+let nine = document.getElementById("nine");
+let four = document.getElementById("four");
+let five = document.getElementById("five");
+let six = document.getElementById("six");
+let one = document.getElementById("one");
+let two = document.getElementById("two");
+let three = document.getElementById("three");
