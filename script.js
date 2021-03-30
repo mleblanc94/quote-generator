e0e468591deb5e03caaacb8fe574771b22085098
Mleblanc94 <leblancmichael94@gmail.com>
2021-03-30 18:52:33 -0400

    buttons added and changes made

--- script.js
+++ script.js
@@ -11,7 +11,19 @@ let apiQuotes = [];
 function newQuote() {
     // Pick a random quote from apiQuotes array
     const quote = apiQuotes[Math.floor(Math.random() * apiQuotes.length)];
-    console.log(quote);
+    // Check if author field is blank and replace it with 'Unknown'
+    if (!quote.author) {
+        authorText.textContent = 'Unknown';
+    } else {
+    authorText.textContent = quote.author;
+    }
+    // Check Quote Length to determine the styling
+    if (quote.text.length > 120) {
+        quoteText.classList.add('long-quote');
+    } else {
+        quoteText.classList.remove('long-quote');
+    }
+    quoteText.textContent = quote.text;
 }
 
 // Get Quotes from API
@@ -25,6 +37,17 @@ async function getQuotes() {
         // Catch Error Here
     }
 }
+
+// Tweet a quote
+
+function tweetQuote() {
+    const twitterUrl = `https://twitter.com/intent/tweet?text=${quoteText.textContent} - ${authorText.textContent}`;
+    window.open(twitterUrl, '_blank');
+}
+
+// Event Listener
+newQuoteBtn.addEventListener("click", newQuote);
+twitterBtn.addEventListener("click", tweetQuote);
 
 // On Load
 getQuotes();
